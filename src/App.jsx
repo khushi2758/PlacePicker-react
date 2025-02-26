bddf5d66f438edd8be9ff0927ab3787a7dea5a21
Khushi Bera <145190251+khushi2758@users.noreply.github.com>
2025-02-26 14:17:57 +0530

    fenish

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState,useEffect } from 'react';
+import { useRef, useState,useEffect,useCallback } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -62,7 +62,8 @@ const [modalIsOpen, setModalIsOpen] = useState()
     localStorage.setItem('selectedPlaces',JSON.stringify([id,...storedIds]));
   }
   //<--------------------------------------------------------------------->
-  function handleRemovePlace() {
+  
+  const handleRemovePlace = useCallback(  function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -70,7 +71,8 @@ const [modalIsOpen, setModalIsOpen] = useState()
     const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [] ;
     localStorage.setItem('selectedPlaces',JSON.stringify(storedIds.filter((id) => id!== selectedPlace.current )));
     
-  }
+  },[]);
+
 
   return (
     <>
@@ -79,6 +81,7 @@ const [modalIsOpen, setModalIsOpen] = useState()
     onCancel={handleStopRemovePlace}
     onConfirm={handleRemovePlace}
   />
+
 </Modal>
 
       <header>
--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,22 @@
+import { useEffect} from "react";
+import ProgressBar from './ProgressBar.jsx';
+const TIMER = 3000;
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
+
+
+
+  useEffect(()=>{
+    console.log("TIMER SET");
+    const timer =  setTimeout(() => {
+      onConfirm();
+    }, TIMER);
+     return() => {
+      console.log("Cleaning up");
+      clearTimeout(timer)
+     }
+  },[onConfirm])
+
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -10,7 +28,8 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
         <button onClick={onConfirm} className="button">
           Yes
         </button>
-      </div>
+        </div>
+      <ProgressBar timer={TIMER}/>
     </div>
   );
 }
